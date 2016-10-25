c6ceea4268104537772aadc9cfda7ec548e38c9c
Abhishek Jain <abhi@noobgames.in>
2016-10-25 06:02:39 +0530

    Add Listener for clicking on offers

---
+++ app/src/main/java/com/abhi/fyberdemo/listeners/OfferClickListener.java
@@ -0,0 +1,13 @@
+package com.abhi.fyberdemo.listeners;
+
+import android.view.View;
+
+import com.abhi.fyberdemo.models.OfferModel;
+
+/**
+ * Created by abhi on 25/10/16.
+ */
+
+public interface OfferClickListener {
+    void OnOfferClick(View view, OfferModel offer);
+}
