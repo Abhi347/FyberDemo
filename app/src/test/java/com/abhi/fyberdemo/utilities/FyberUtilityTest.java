2c76930e82b3bac0f7bd3dd6fb2011c2d4ff72d1
Abhishek Jain <abhi@noobgames.in>
2016-10-28 01:32:56 +0530

    Add UnitTest for FyberUtility class

--- app/src/test/java/com/abhi/fyberdemo/utilities/FyberUtilityTest.java
+++ app/src/test/java/com/abhi/fyberdemo/utilities/FyberUtilityTest.java
@@ -1,6 +1,6 @@
-package com.abhi.fyberdemo;
+package com.abhi.fyberdemo.utilities;
 
-import com.abhi.fyberdemo.utilities.FyberUtility;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
@@ -9,13 +9,24 @@ import java.util.HashMap;
 import static junit.framework.Assert.assertEquals;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Created by abhi on 28/10/16.
  */
-public class FyberUtilityUnitTest {
+public class FyberUtilityTest {
     @Test
-    public void testConvertParamsToSHA1Hash() {
+    public void getOffersHashMap() throws Exception {
+        HashMap<String, String> paramsMap = FyberUtility.getOffersHashMap("1234",
+                "12b2",
+                "MainActivity",
+                "batguy",
+                "4.1.2",
+                "EN",
+                1477465419L,
+                false);
+        Assert.assertNotNull(paramsMap);
+    }
+
+    @Test
+    public void getSortedParams() throws Exception {
         HashMap<String, String> paramsMap = FyberUtility.getOffersHashMap("1234",
                 "12b2",
                 "MainActivity",
@@ -27,8 +38,12 @@ public class FyberUtilityUnitTest {
 
         String params = FyberUtility.getSortedParams(paramsMap);
         String paramsExpected = "appid=1234&format=json&google_ad_id=12b2&google_ad_id_limited_tracking_enabled=false&ip=109.235.143.113&locale=EN&offer_types=112&os_version=4.1.2&pub0=MainActivity&timestamp=1477465419&uid=batguy";
-        assertEquals("Parameters Sorting failed", paramsExpected, params);
+        Assert.assertEquals("Parameters Sorting failed", paramsExpected, params);
+    }
 
+    @Test
+    public void getHashKey() throws Exception {
+        String params = "appid=1234&format=json&google_ad_id=12b2&google_ad_id_limited_tracking_enabled=false&ip=109.235.143.113&locale=EN&offer_types=112&os_version=4.1.2&pub0=MainActivity&timestamp=1477465419&uid=batguy";
         String hashString = FyberUtility.getHashKey(params, "123456789");
         String hashStringExpected = "d79dd6aef01987ffd5beaea9ce43103d729f3f64";
         assertEquals("Parameters conversion to SHA1 hash failed", hashStringExpected,hashString);
